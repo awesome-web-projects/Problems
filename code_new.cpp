70cbbd6696b63982adf858caab76b408601a5387
Sumit Lahiri <lahiri.devs@gmail.com>
2020-11-16 13:36:15 +0530

    Create code_new.cpp

---
+++ code_new.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <unordered_map>
+#include <memory>
+#include <utility>
+#include <set>
+#include <unordered_set>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <future>
+#include <functional>
+#include <algorithm>
+#include <regex>
+
+typedef long long int lli;
+typedef unsigned long long int ulli;
+
+int main(int argc, char **argv, char **envp)
+{
+    return 0;
+}

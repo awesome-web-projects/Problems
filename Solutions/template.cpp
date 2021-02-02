51fdf8065cadb593afd13f0029ef79cf70e03a8c
Sumit Lahiri <lahiri.devs@gmail.com>
2021-02-02 11:14:42 +0530

    Template Updated

--- Solutions/template.cpp
+++ Solutions/template.cpp
@@ -1,8 +1,29 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <unordered_map>
+#include <memory>
+#include <utility>
+#include <set>
+#include <unordered_set>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <future>
+#include <functional>
+#include <algorithm>
+#include <regex>
+
+typedef long long int lli;
+typedef unsigned long long int ulli;
 
 int main(int argc, char **argv, char **envp)
 {
-    /// HACK : Increases I/O speed. How? hehhe..
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
     std::cout.tie(NULL);
